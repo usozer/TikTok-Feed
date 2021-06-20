8d7e6a96002dfed817a9ef92284861e3780b66a7
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-20 00:12:13 -0500

    Grab embedding from tiktok API

---
+++ src/embed.py
@@ -0,0 +1,29 @@
+from typing import List
+
+import requests
+from pyppeteer import launch
+from pyppeteer.browser import Browser
+
+
+
+
+async def get_embed_html(link: str, browser: Browser):
+
+    page = await browser.newPage()
+    init_response = await page.goto(link)
+    videourl = init_response.request.url
+
+    r = requests.get("https://www.tiktok.com/oembed?url=" + videourl)
+
+    return r.json()["html"]
+
+
+async def embed(links: List[str]):
+    browser = await launch()
+
+    html_elements = []
+    for link in links:
+        element = await get_embed_html(link, browser)
+        html_elements.append(element)
+
+    return html_elements
