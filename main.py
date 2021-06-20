b28fb645649fa8b52fcd72c8aba515835613b014
Uygar Sozer <uygarsozer2021@u.northwestern.edu>
2021-06-20 18:57:52 -0500

    Simplified request to obtain the most recent videos Now centralized at a single get request separate from index

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from re import template
+import logging
+
+import yaml
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,33 +8,62 @@ from fastapi.templating import Jinja2Templates
 
 from src.embed import embed
 from src.session import SessionManager
+from src.initialize import TikTok
+
+logger = logging.getLogger(__name__)
+
+with open("config/config.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-session = SessionManager("sqlite:///clean.db")
+logger.info("Initialized app")
+
+app.mount(
+    config["app"]["static_folder"],
+    StaticFiles(directory="static"),
+    name="static",
+)
+session = SessionManager(config["app"]["db_path"])
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    initial = session.get_latest(10)
+    """Index page"""
+    logger.info("Accessed main page")
 
-    embeds = await embed(initial)
-
-    return templates.TemplateResponse(
-        "index.html", {"request": request, "embeds": embeds}
-    )
+    # Render template with the embedding
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/boot")
 async def html_element():
-    initial = session.get_latest(10)
+    """Returning the most recent videos"""
+    no_videos = config["app"]["num_tiktoks"]
+    logger.debug("Loading %i videos", no_videos)
+    initial = session.get_latest(no_videos)
 
+    # Get embedding code for each video as a list
     embeds = await embed(initial)
+    logger.debug("Returning %i videos", len(initial))
 
+    # Return embedding code as a list
     return embeds
+
 
 @app.post("/newembed")
 async def post_link(request: Request):
-    pass
+    """Post new link to database"""
+    link = await request.json()
+    logger.info(
+        "Received new insertion request: %s, %i",
+        link["shortlink"],
+        link["timestamp"],
+    )
+    session.add_to_db(
+        TikTok(shortlink=link["shortlink"], timestamp=link["timestamp"])
+    )
+    
+    return "success"
